ec917e1494d6396e405e1f129ac65941caa84fab
sakil-sagor <sakilsagor76543@gmail.com>
2022-02-24 15:43:07 +0600

    fix product bug

--- src/Hooks/useProducts.js
+++ src/Hooks/useProducts.js
@@ -8,7 +8,7 @@ const useProducts = () => {
     useEffect(() => [
         fetch('https://time-zone-78.herokuapp.com/products')
             .then(res => res.json())
-            .then(data => setProducts(data))
+            .then(data => setProducts(data.products))
     ], [])
     return [products, setProducts]
 
